28b7142f9217f9014d6cbd3d3f8c772bbdd8f0a6
LaliaStudio <laliastudio@gmail.com>
2024-01-08 23:57:46 +0100

    homepage

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,9 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    console.log(data);
+    const { name, portrait, city, country , tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
+    console.log(picture);
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +11,20 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const geographicZone = document.createElement( 'span' );
+        geographicZone.classList.add("geographic");
+        geographicZone.textContent = city + " , " + country;
+        const taglineZone = document.createElement( 'p' );
+        taglineZone.textContent = tagline;
+        taglineZone.classList.add("tagline");
+        const priceZone = document.createElement( 'span' );
+        priceZone.textContent = price+"€/jour";
+        priceZone.classList.add("price");
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(geographicZone);
+        article.appendChild(taglineZone);
+        article.appendChild(priceZone);
         return (article);
     }
     return { name, picture, getUserCardDOM }
